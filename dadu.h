a3d6314f46bd8c57fb6ebe6e2fa8915bf91d67fd
lolipopper <njruntuwene@gmail.com>
2015-11-10 01:46:14 +0900

    Remove function

--- dadu.h
+++ dadu.h
@@ -14,12 +14,4 @@ typedef struct {
 #define Dice1(D) (D).dice1
 #define Dice2(D) (D).dice2
 
-int roll()
-{
-	Dadu D;
-	Dice1(D)=(rand()%6)+1;
-	Dice2(D)=(rand()%6)+1;
-	return Dice1(D)+Dice2(D);
-}
-
 #endif
