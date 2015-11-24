4d4c290be0cb160cf434b110badc61e5ff38a2bf
lolipopper <njruntuwene@gmail.com>
2015-11-25 04:43:25 +0900

    Fix kota

--- kota.h
+++ kota.h
@@ -1,13 +1,16 @@
 #ifndef KOTA_H
 #define KOTA_H
+#include "boolean.h"
+#include "mesinkata.h"
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "other.h"
-
-// KONSTANTA
+//KONSTANTA
 #define IdxMax 32
 #define IdxMin 1
 
-// DEFINISI TIPE
+//DEFINISI TIPE
 typedef int IdxType;
 typedef struct {
 	Kata name;
@@ -15,21 +18,27 @@ typedef struct {
 	boolean rekreasi;
 	int block;
 	int level;
-	char owner;
+	Player owner;
 	boolean LightOff;
 } Kota;
 
 typedef struct {
-	Kota TK[IdxMax+1];
+	ElType TK[IdxMax+1];
 } TabKota;
 
-// SELEKTOR
-#define NamaKota(Kota,i) (Kota).TK[(i)].name
-#define Price(Kota,i) (Kota).TK[(i)].price
-#define isRekreasi(Kota,i) (Kota).TK[(i)].rekreasi
-#define Block(Kota,i) (Kota).TK[(i)].block
-#define Level(Kota,i) (Kota).TK[(i)].level
-#define Owner(Kota,i) (Kota).TK[(i)].owner
-#define LightOff(Kota,i) (Kota).TK[(i)].owner
+//SELEKTOR
+#define NamaKota(Kota) (Kota).name
+#define Price(Kota) (Kota).price
+#define IsRekreasi(Kota) (Kota).rekreasi
+#define Block(Kota) (Kota).block
+#define Level(Kota) (Kota).level
+#define Owner(Kota) (Kota).owner
+#define IsLightOff(Kota) (Kota).LightOff
+#define LengthNama(Kota) (Kota).name.Length
+
+void buy(Player *P, Kota *K);
+void payRent(Player *P,Kota K);
+void upgrade(Kota *K);
+int UpgradePrice(Kota K);
 
 #endif
